19b98092cebdd5404c387af76d0d03e5f2e24585
marciovz <marciovz@yahoo.com.br>
2023-06-19 17:47:14 -0300

    implementado página de posts

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,11 +1,28 @@
-import { PostHeaderCard } from '../../components/PostHeaderCard'
+import ReactMarkdown from 'react-markdown'
+import { useParams } from 'react-router-dom'
 
-import { Container } from './styles'
+import { PostHeaderCard } from '../../components/PostHeaderCard'
+import { usePosts } from '../../context/postsContext'
+
+import { Container, Content } from './styles'
 
 export function Post() {
+  const { id } = useParams()
+  const { profile, issues, isLoadingIssues, isLoadingProfile } = usePosts()
+  const isLoading = isLoadingIssues || isLoadingProfile
+
+  const issue = issues.items.find((item) => String(item.id) === id)
+
   return (
     <Container>
-      <PostHeaderCard style={{ marginTop: '-5.5rem' }} />
+      <PostHeaderCard
+        profile={profile}
+        title={issue?.title}
+        publicationDate={issue?.createdAt}
+        isLoading={isLoading}
+        style={{ marginTop: '-5.5rem' }}
+      />
+      <Content>{issue && <ReactMarkdown>{issue?.body}</ReactMarkdown>}</Content>
     </Container>
   )
 }
--- src/pages/Post/styles.ts
+++ src/pages/Post/styles.ts
@@ -4,3 +4,72 @@ export const Container = styled.div`
   width: 864px;
   margin: 0 auto 14rem;
 `
+
+export const Content = styled.div`
+  padding: 2.5rem 0;
+
+  > h2 {
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme['base-title']};
+    line-height: 2.5rem;
+    margin: 1.5rem 0;
+    text-transform: uppercase;
+  }
+
+  > h3 {
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme['base-subtitle']};
+    line-height: 2rem;
+    margin: 1.25rem 0;
+  }
+
+  > h4 {
+    font-size: 1.125rem;
+    color: ${({ theme }) => theme['base-subtitle']};
+    line-height: 1.5rem;
+    margin: 1.125rem 0;
+  }
+
+  > p {
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-text']};
+    line-height: 1.625rem;
+    margin: 1rem 0;
+  }
+
+  > p:has(img) {
+    text-align: center;
+  }
+
+  a {
+    margin-top: 1.625rem;
+    font-size: inherit;
+    color: ${({ theme }) => theme.blue};
+    line-height: 1.625rem;
+  }
+
+  img {
+    max-width: 100%;
+    margin: 1rem auto;
+  }
+
+  ul {
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-text']};
+    line-height: 1.625rem;
+    margin-left: 1rem;
+  }
+
+  li {
+    margin: 1rem 0;
+  }
+
+  pre {
+    margin: 1rem 0;
+    padding: 1.5rem;
+    font-size: 1rem;
+    color: ${({ theme }) => theme['base-span']};
+    background-color: ${({ theme }) => theme['base-label']};
+    border-radius: 10px;
+  }
+`
